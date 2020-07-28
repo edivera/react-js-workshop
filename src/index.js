903bfdede3f0ee31c729b04157c00ce5682ad695
Edilio Vera <ediliovera97@gmail.com>
2020-07-28 17:24:46 -0500

    added route for App.js under the heading, through a switch in the browser router

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import App from './components/App';
 import Heading from './components/Heading';
+import PeopleList from './components/PeopleList';
 
+// definitely need the react dom
+// we can add a browserrouter with switches which have
+// different routes
+// {App} two way bind
 ReactDOM.render(
   <BrowserRouter>
-  <Heading></Heading>
+    <Heading/>
+    <Switch>
+      <Route exact path="/" component={App} />
+    </Switch>
   </BrowserRouter>,
   document.getElementById('root')
 );
