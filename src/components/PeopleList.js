af6aef2c2f35bd83ff3686aa9a9dda738df848ee
Edilio Vera <ediliovera97@gmail.com>
2020-07-28 21:07:53 -0500

    added the css for people

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
 
 export class PeopleList extends Component {
+    
+    renderPeople = () => {
+        if(this.props.people !== undefined) {
+            return this.props.people.map(person => {
+                return(
+                    <div key={person.id} className="singleProfilePicContainer">
+                        <img className="profilePic" src={`Albums/${person.name}/${person.profile_pic}.jpeg`} 
+                            />
+                        <div>
+                            <h1 className="info">{person.name}</h1>
+                            {
+                                person.online_status === "on" &&
+                                    <img className="live" src="Albums/live.svg" />
+                            }
+                        </div>
+                    </div>
+                )
+            });
+        }
+    }
+    
     render() {
         console.log(this.props.people);
         return (
             <div>
-                <h3>I am from people list</h3>
+                {this.renderPeople()}
             </div>
         )
     }
